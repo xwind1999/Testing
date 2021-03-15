763e6580fad397c01d49ba4dfbdd1568b23db5c2
xwind1999 <playgoodgame7@gmail.com>
2021-03-15 11:05:17 +0700

    Integration Test in Ubuntu

--- tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
@@ -1,11 +1,8 @@
 <?php
 
-
 namespace App\Tests\AppBundle\Service;
 
-
 use App\Entity\Dinosaur;
-use App\Entity\Enclosure;
 use App\Entity\Security;
 use App\Service\EnclosureBuilderService;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
@@ -14,55 +11,39 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
 {
-    public function setUp(): void
-    {
-        self:self::bootKernel();
-
-        $this->truncateEntities([
-            Enclosure::class,
-            Security::class,
-            Dinosaur::class,
-        ]);
-    }
-
-    public function testItBuildsEnclosureWithDefaultSpecifications()
+    public function setUp() :void
     {
         self::bootKernel();
+        $this->truncateEntities();
+    }
+    public function testItBuildsEnclosureWithDefaultSpecifications()
+    {
         $enclosureBuilderService = self::$kernel->getContainer()
-            ->get(EnclosureBuilderService::class);
-
+            ->get('test.'.EnclosureBuilderService::class);
         $enclosureBuilderService->buildEnclosure();
-
-        $entityManger = self::$kernel->getContainer()
-            ->get('doctrine')
-            ->getManager();
-
-        $count = (int) $entityManger->getRepository(Security::class)
+        $em = $this->getEntityManager();
+        $count = (int) $em->getRepository(Security::class)
             ->createQueryBuilder('s')
-            ->select('Count(s.id)')
+            ->select('COUNT(s.id)')
             ->getQuery()
             ->getSingleScalarResult();
-
-        $this->assertSame(1, $count, "Amount of security systems is not the same");
-
-        $count = (int) $entityManger->getRepository(Dinosaur::class)
+        $this->assertSame(1, $count, 'Amount of security systems is not the same');
+        $count = (int) $em->getRepository(Dinosaur::class)
             ->createQueryBuilder('d')
             ->select('COUNT(d.id)')
             ->getQuery()
             ->getSingleScalarResult();
         $this->assertSame(3, $count, 'Amount of dinosaurs is not the same');
     }
-
-    private function truncateEntities(array $entities)
+    private function truncateEntities()
     {
-       $purger = new ORMPurger($this->getEntityManager());
-       $purger->purge();
+        $purger = new ORMPurger($this->getEntityManager());
+        $purger->purge();
     }
-
     /**
      * @return EntityManager
      */
-    public function getEntityManager(): EntityManager
+    private function getEntityManager()
     {
         return self::$kernel->getContainer()
             ->get('doctrine')
